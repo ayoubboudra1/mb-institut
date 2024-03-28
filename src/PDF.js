6a88f08bdffebe7e33eb8d6e8df8368ada48f9b8
ayoubboudra1 <ayoub.boudra1@gmail.com>
2024-03-28 13:52:26 +0000

    fix bg-color

--- src/PDF.js
+++ src/PDF.js
@@ -143,7 +143,7 @@ function PDF({downloadPdf,index,setListData,listData,deleteMois}) {
                         <h5>{dateFin?convertDateFormat(dateFin):''}</h5>
                     </div>
                 </div>
-                <div class="card" style={{boxShadow: '0 0 10px rgba(0,0,0,0.1)',backgroundColor:'white'}}>
+                <div class="" style={{boxShadow: '0 0 10px rgba(0,0,0,0.1)',backgroundColor:'white'}}>
                     <div class="icon bg--red">
                         <svg xmlns="http://www.w3.org/2000/svg" style={{color:"white"}} width="22" height="22" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
